6f010527321f365174019b6dc0978e5cb49367e1
Sanath K S <sanathselvakumaran@gmail.com>
2026-05-18 07:14:41 +0000

    How do I change the icon in PWA app ?

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -12,12 +12,12 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#2855BF',
     icons: [
       {
-        src: 'https://picsum.photos/seed/kanakku/192/192',
+        src: 'https://picsum.photos/seed/wallet-app/192/192',
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: 'https://picsum.photos/seed/kanakku/512/512',
+        src: 'https://picsum.photos/seed/wallet-app/512/512',
         sizes: '512x512',
         type: 'image/png',
       },
